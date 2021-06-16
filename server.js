f58dce8dfffac02ddff15430ec5fe48c2fb80b72
ismael <albertismael7@gmail.com>
2021-06-15 21:54:39 -0300

    _

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
+    const items = [
+{
+    title: "stacks",
+    message: "principais stacks"
+}
+{
+    title: "front end",
+    message: "HTML, CSS, JS, React.Js, Vue.js"
+}
+
+{
+    title: "back end"
+    message: ''
+}
+    ];
     res.render("pages/index");
 })
 
